54db4a5bbafb3dd656032ac1fc562704862680c4
Emirhan KALEM <emirhan.kalem@cubetron.com.tr>
2023-11-05 20:34:01 +0300

    Request Validation Behaviour

---
+++ Microservice.Core.Packages.Application/Pipelines/Validation/ValidationTool.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.Core.Packages.Application.Pipelines.Validation;
+
+public class ValidationTool
+{
+    public static void Validate(IValidator validator, object entity)
+    {
+        ValidationContext<object> context = new(entity);
+        ValidationResult result = validator.Validate(context);
+        if (!result.IsValid) throw new ValidationException(result.Errors);
+    }
+}
