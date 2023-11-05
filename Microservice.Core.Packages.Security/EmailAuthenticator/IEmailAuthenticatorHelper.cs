5acb0f77775e0e5af78b2cc18905adfc31010f60
Emirhan KALEM <emirhan.kalem@cubetron.com.tr>
2023-11-05 18:51:54 +0300

    Email authenticator helper

---
+++ Microservice.Core.Packages.Security/EmailAuthenticator/IEmailAuthenticatorHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.Core.Packages.Security.EmailAuthenticator;
+
+public interface IEmailAuthenticatorHelper
+{
+    public Task<string> CreateEmailActivationKey();
+    public Task<string> CreateEmailActivationCode();
+}
