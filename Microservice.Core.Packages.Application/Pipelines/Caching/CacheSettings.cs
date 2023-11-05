49c43186fc138bc2ca283bfd88ac0af39fe56e39
Emirhan KALEM <emirhan.kalem@cubetron.com.tr>
2023-11-05 20:30:44 +0300

    Caching behaviour

---
+++ Microservice.Core.Packages.Application/Pipelines/Caching/CacheSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.Core.Packages.Application.Pipelines.Caching;
+
+public class CacheSettings
+{
+    public int SlidingExpiration { get; set; }
+}
---
+++ Microservice.Core.Packages.Application/Pipelines/Caching/ICacheRemoverRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.Core.Packages.Application.Pipelines.Caching;
+
+public interface ICacheRemoverRequest
+{
+    bool BypassCache { get; }
+    string CacheKey { get; }
+}
