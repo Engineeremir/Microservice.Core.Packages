8c373da998b5d04bb9c54455660b0e92ee0a9e70
Emirhan KALEM <emirhan.kalem@cubetron.com.tr>
2023-11-05 18:27:21 +0300

    Persistence layer.

---
+++ Microservice.Core.Packages.Persistence/Repositories/EntityFramework/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.Core.Packages.Persistence.Repositories.EntityFramework;
+
+public class Entity
+{
+    public int Id { get; set; }
+    public Entity()
+    {
+        
+    }
+
+    public Entity(int id):this()
+    {
+        Id = id;
+    }
+}
---
+++ Microservice.Core.Packages.Persistence/Repositories/EntityFramework/IQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.Core.Packages.Persistence.Repositories.EntityFramework;
+
+public interface IQuery<T>
+{
+    IQueryable<T> Query();
+}
