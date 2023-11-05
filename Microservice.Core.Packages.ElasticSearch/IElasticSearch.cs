e5ad9c15114be627ccb7840fca8421d41c7777b2
Emirhan KALEM <emirhan.kalem@cubetron.com.tr>
2023-11-05 19:29:46 +0300

    Elastich search manager

---
+++ Microservice.Core.Packages.ElasticSearch/IElasticSearch.cs
@@ -0,0 +1,24 @@
+﻿using Microservice.Core.Packages.ElasticSearch.Models;
+using Nest;
+
+namespace Microservice.Core.Packages.ElasticSearch;
+
+public interface IElasticSearch
+{
+    Task<IElasticSearchResult> CreateNewIndexAsync(IndexModel indexModel);
+    Task<IElasticSearchResult> InsertAsync(ElasticSearchInsertUpdateModel model);
+    Task<IElasticSearchResult> InsertManyAsync(string indexName, object[] items);
+    IReadOnlyDictionary<IndexName, IndexState> GetIndexList();
+
+    Task<List<ElasticSearchGetModel<T>>> GetAllSearch<T>(SearchParameters parameters)
+        where T : class;
+
+    Task<List<ElasticSearchGetModel<T>>> GetSearchByField<T>(SearchByFieldParameters fieldParameters)
+        where T : class;
+
+    Task<List<ElasticSearchGetModel<T>>> GetSearchBySimpleQueryString<T>(SearchByQueryParameters queryParameters)
+        where T : class;
+
+    Task<IElasticSearchResult> UpdateByElasticIdAsync(ElasticSearchInsertUpdateModel model);
+    Task<IElasticSearchResult> DeleteByElasticIdAsync(ElasticSearchModel model);
+}
